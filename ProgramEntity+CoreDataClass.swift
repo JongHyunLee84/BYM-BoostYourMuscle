001579e38da5cdf62e674a43163da82709260835
JongHyunLee84 <whdgus3697@naver.com>
2023-07-01 15:35:08 +0900

    fix: delete some coredata files and unify

--- ProgramEntity+CoreDataClass.swift
+++ ProgramEntity+CoreDataClass.swift
@@ -13,3 +13,41 @@ import CoreData
 public class ProgramEntity: NSManagedObject {
 
 }
+
+extension ProgramEntity {
+
+    @nonobjc public class func fetchRequest() -> NSFetchRequest<ProgramEntity> {
+        return NSFetchRequest<ProgramEntity>(entityName: "ProgramEntity")
+    }
+
+    @NSManaged public var title: String?
+    @NSManaged public var programToExercise: Set<ExerciseEntity>?
+
+    // 내부에 있는 id를 갖고 정렬하는 코드인데 정확히는 잘 모르겠음
+     public var exerciseArray: [ExerciseEntity] {
+         return programToExercise!.sorted {
+             return $0.id > $1.id
+         }
+     }
+}
+
+// MARK: Generated accessors for programToExercise
+extension ProgramEntity {
+
+    @objc(addProgramToExerciseObject:)
+    @NSManaged public func addToProgramToExercise(_ value: ExerciseEntity)
+
+    @objc(removeProgramToExerciseObject:)
+    @NSManaged public func removeFromProgramToExercise(_ value: ExerciseEntity)
+
+    @objc(addProgramToExercise:)
+    @NSManaged public func addToProgramToExercise(_ values: NSSet)
+
+    @objc(removeProgramToExercise:)
+    @NSManaged public func removeFromProgramToExercise(_ values: NSSet)
+
+}
+
+extension ProgramEntity : Identifiable {
+
+}
--- PsetEntity+CoreDataClass.swift
+++ PsetEntity+CoreDataClass.swift
@@ -13,3 +13,21 @@ import CoreData
 public class PsetEntity: NSManagedObject {
 
 }
+
+extension PsetEntity {
+
+    @nonobjc public class func fetchRequest() -> NSFetchRequest<PsetEntity> {
+        return NSFetchRequest<PsetEntity>(entityName: "PsetEntity")
+    }
+
+    @NSManaged public var check: Bool
+    @NSManaged public var reps: Int64
+    @NSManaged public var weight: Double
+    @NSManaged public var id: Int64
+    @NSManaged public var psetToExercise: ExerciseEntity?
+
+}
+
+extension PsetEntity : Identifiable {
+
+}
