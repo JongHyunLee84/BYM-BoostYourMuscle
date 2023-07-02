af27d5ea7cfa3c1221efaed0320c5c9f8a4051f9
JongHyunLee84 <whdgus3697@naver.com>
2023-07-02 13:48:43 +0900

    style: change location of some coredatafiles again

---
+++ Workouter/Models/CoreDataEntities/ExerciseEntity+CoreDataClass.swift
@@ -0,0 +1,57 @@
+//
+//  ExerciseEntity+CoreDataClass.swift
+//  BYM
+//
+//  Created by 이종현 on 2023/04/06.
+//
+//
+
+import Foundation
+import CoreData
+
+@objc(ExerciseEntity)
+public class ExerciseEntity: NSManagedObject {
+
+}
+
+extension ExerciseEntity {
+
+    @nonobjc public class func fetchRequest() -> NSFetchRequest<ExerciseEntity> {
+        return NSFetchRequest<ExerciseEntity>(entityName: "ExerciseEntity")
+    }
+
+    @NSManaged public var name: String?
+    @NSManaged public var rest: Int64
+    @NSManaged public var target: String?
+    @NSManaged public var id: Int64
+    @NSManaged public var exerciseToProgram: ProgramEntity?
+    @NSManaged public var exerciseToPset: Set<PsetEntity>?
+    
+    public var psetArray: [PsetEntity] {
+        return exerciseToPset!.sorted {
+            return $0.id > $1.id
+        }
+    }
+
+}
+
+// MARK: Generated accessors for exerciseToPset
+extension ExerciseEntity {
+
+    @objc(addExerciseToPsetObject:)
+    @NSManaged public func addToExerciseToPset(_ value: PsetEntity)
+
+    @objc(removeExerciseToPsetObject:)
+    @NSManaged public func removeFromExerciseToPset(_ value: PsetEntity)
+
+    @objc(addExerciseToPset:)
+    @NSManaged public func addToExerciseToPset(_ values: NSSet)
+
+    @objc(removeExerciseToPset:)
+    @NSManaged public func removeFromExerciseToPset(_ values: NSSet)
+
+}
+
+extension ExerciseEntity : Identifiable {
+
+}
